ad4d6dade6eabcd838f58db864fd094bf1a0de5e
AnkitaPadhy0 <ankitapadhy098@gmail.com>
2025-07-04 11:39:49 +0530

    Update script.js

--- Task_01/script.js
+++ Task_01/script.js
@@ -1,8 +1,3 @@
-window.addEventListener('scroll', function () {
-  const navbar = document.getElementById('navbar');
-  if (window.scrollY > 50) {
-    navbar.classList.add('scrolled');
-  } else {
-    navbar.classList.remove('scrolled');
-  }
-});
+function scrollToSection() {
+  document.getElementById("about").scrollIntoView({ behavior: "smooth" });
+}
